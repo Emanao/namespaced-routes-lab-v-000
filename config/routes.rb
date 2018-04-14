798011aa9f557668e3b45d07fa1b08a242f58969
Eva Navarror <emanao@me.com>
2018-04-14 22:17:25 +0000

    Saves preferences

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :preferences
+    resources :preferences, only: [:index, :update]
   end
   resources :artists do
     resources :songs, only: [:index, :show]
